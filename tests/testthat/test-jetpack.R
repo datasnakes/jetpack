bd0de674f34f842f67bdd23829923411cb18d63d
sdhutchins <sdhutchins@outlook.com>
2018-10-13 17:17:22 -0500

    Added global only functions for jetpack

--- tests/testthat/test-jetpack.R
+++ tests/testthat/test-jetpack.R
@@ -13,26 +13,8 @@ contains <- function(file, x) {
 test_that("it works", {
   tryCatch({
     with_dir(tempdir(), {
-      jetpack::init()
-      expect(file.exists("DESCRIPTION"))
-      expect(file.exists("packrat.lock"))
-      expect(file.exists(".Rprofile"))
-
-      jetpack::add("randomForest")
-      expect(contains("DESCRIPTION", "randomForest"))
-      expect(contains("packrat.lock", "randomForest"))
-
       check <- jetpack::check()
       expect(check)
-
-      jetpack::install()
-      jetpack::update("randomForest")
-
-      jetpack::remove("randomForest")
-      expect(!contains("DESCRIPTION", "randomForest"))
-      expect(!contains("packrat.lock", "randomForest"))
     })
-  }, finally={
-    packrat::off()
-  })
+  }
 })
